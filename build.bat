002e51ef0fde9d07d696e1db164841c8b6497bf1
Miranac <miramac@gmx.net>
2012-09-26 19:30:19 +0200

    path fixed

--- build.bat
+++ build.bat
@@ -8,8 +8,8 @@ node .\build >> build.log
 
 :: compile bootstrap CSS (could be integrated into the build-script...)
 echo compiling bootstrap css...
-node .\build\node_modules\less\bin\lessc .\bootstrap.override\less\bootstrap.less > .\src\css\bootstrap.css
-node .\build\node_modules\less\bin\lessc .\bootstrap.override\less\bootstrap.less > .\src\css\bootstrap.min.css --compress
+node .\node_modules\less\bin\lessc .\bootstrap.override\less\bootstrap.less > .\src\css\bootstrap.css
+node .\node_modules\less\bin\lessc .\bootstrap.override\less\bootstrap.less > .\src\css\bootstrap.min.css --compress
 
 
 :: create new zip-ball in bin dir (should be integrated into the build-script!)
