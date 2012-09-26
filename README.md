db9ee4d648b51d12d3805c5e08adf1c1c84a8373
Miranac <miramac@gmx.net>
2012-09-26 17:44:13 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-joomla-bootstrap-template
-=========================
+Basic Joomla 2.5 Template
+==================================================
 
+Overview
+--------------------------------------
 Joomla 2.5 Template based on Twitters Bootstrap CSS Framework
+
+Basic default template with bootstrap 2.1, jquery 1.8
+
+
+Inspired by: https://github.com/Bloggerschmidt/Blank-Template 
+
+
---
+++ build.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+echo new log > build.log
+
+:: Cleanup, copy jobs, concat & minify bootsrap.js
+echo building package...
+node .\build >> build.log
+
+:: compile bootstrap CSS (could be integrated into the build-script...)
+echo compiling bootstrap css...
+node .\build\node_modules\less\bin\lessc .\bootstrap.override\less\bootstrap.less > .\src\css\bootstrap.css
+node .\build\node_modules\less\bin\lessc .\bootstrap.override\less\bootstrap.less > .\src\css\bootstrap.min.css --compress
+
+
+:: create new zip-ball in bin dir (should be integrated into the build-script!)
+echo creating joomla template .ZIP file
+.\tools\7-Zip\7z -tzip a .\bin\joomla-bootstrap-template.zip .\src\* >> build.log
+
+::pause
