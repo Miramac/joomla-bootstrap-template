634708cce13711ab7cbd20fed225eb1a0303fa83
Miranac <miramac@gmx.net>
2012-09-27 20:11:32 +0200

    Removed bootstrap icons

--- src/index.php
+++ src/index.php
@@ -12,7 +12,7 @@ $app = JFactory::getApplication();
 $doc = JFactory::getDocument(); 
 $params = $app->getParams();
 
-$this->setGenerator(null); // remove the generator tag
+// // $this->setGenerator(null); // remove the generator tag
 
 // unset scripts, to place them at the end of the document
 /* Joomla JS-files has to be in the head tag
@@ -42,11 +42,6 @@ unset($doc->_scripts[$this->baseurl.'/media/system/js/caption.js']);
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/ico/apple-touch-icon-57-precomposed.png">
-
 	</head>
 
 	<body>
